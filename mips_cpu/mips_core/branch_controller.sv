cf7d4f25b4ba0af52fa46a1c392f0f3a0b8ce2b2
Trivikram Choudhury <tchoudhury@ucsd.edu>
2025-02-01 02:15:45 +0000

    Added 16-bit gshare predictor and modified Makefile to allow loop unrolling in initialization

--- mips_cpu/mips_core/branch_controller.sv
+++ mips_cpu/mips_core/branch_controller.sv
@@ -25,7 +25,7 @@ module branch_controller (
 	logic request_prediction;
 
 	// Change the following line to switch predictor
-	branch_predictor_always_not_taken PREDICTOR (
+	branch_predictor_gshare PREDICTOR (
 		.clk, .rst_n,
 
 		.i_req_valid     (request_prediction),
@@ -130,4 +130,89 @@ module branch_predictor_2bit (
 		o_req_prediction = counter[1] ? TAKEN : NOT_TAKEN;
 	end
 
+endmodule
+
+module branch_predictor_gshare (
+	input clk,    // Clock
+	input rst_n,  // Synchronous reset active low
+
+	// Request
+	input logic i_req_valid,
+	input logic [`ADDR_WIDTH - 1 : 0] i_req_pc,
+	input logic [`ADDR_WIDTH - 1 : 0] i_req_target,
+	output mips_core_pkg::BranchOutcome o_req_prediction,
+
+	// Feedback
+	input logic i_fb_valid,
+	input logic [`ADDR_WIDTH - 1 : 0] i_fb_pc,
+	input mips_core_pkg::BranchOutcome i_fb_prediction,
+	input mips_core_pkg::BranchOutcome i_fb_outcome
+);
+localparam COUNTER_WIDTH = 2;
+localparam INDEX_WIDTH = 16;
+
+logic [INDEX_WIDTH-1:0] index_read; 
+logic [INDEX_WIDTH-1:0] index_write; 
+logic [INDEX_WIDTH-1:0] ghr;
+// logic [INDEX_WIDTH-1:0] old_ghr;
+logic [COUNTER_WIDTH-1:0] current_counter;
+
+// logic [1:0] counter;
+
+logic [COUNTER_WIDTH-1:0] counter_regs[2**INDEX_WIDTH];
+
+	task incr;
+		begin
+			if (counter_regs[index_write] != 2'b11)
+				counter_regs[index_write] <= counter_regs[index_write] + 2'b01;
+		end
+	endtask
+
+	task decr;
+		begin
+			if (counter_regs[index_write] != 2'b00)
+				counter_regs[index_write] <= counter_regs[index_write] - 2'b01;
+		end
+	endtask
+
+
+	always_ff @(posedge clk)
+	begin
+		if(~rst_n)
+		begin
+			for (int i=0; i<2**INDEX_WIDTH; i++) 
+			begin
+			counter_regs[i] <= 2'b10; //Default: weakly taken
+			end
+		end
+		else
+		begin
+		index_write <= index_read;
+
+		if (i_fb_valid)
+		begin
+
+				case (i_fb_outcome)
+					NOT_TAKEN: decr();
+					TAKEN:     incr();
+				endcase
+
+				for (int i=1; i<INDEX_WIDTH; i++)
+				begin
+					ghr[i] <= ghr[i-1];
+				end
+			ghr[0] <= i_fb_outcome;
+		end
+		else begin
+			ghr <= ghr;
+		end
+		end
+	end
+
+	always_comb
+	begin
+
+		index_read = i_req_pc[INDEX_WIDTH-1:0] ^ ghr;
+		o_req_prediction = counter_regs[index_read][1] ? TAKEN : NOT_TAKEN;
+	end
 endmodule
