d8ed5b33963bc1634192871a80ffefe263e5eeae
Trivikram Choudhury <tchoudhury@ucsd.edu>
2025-03-18 02:56:38 +0000

    Thread ID passed through pipeline

--- mips_cpu/mips_core/i_cache.sv
+++ mips_cpu/mips_core/i_cache.sv
@@ -23,216 +23,264 @@
  *
  * See wiki page "Synchronous Caches" for details.
  */
-`include "mips_core.svh"
+ `include "mips_core.svh"
 
-module i_cache #(
-	parameter INDEX_WIDTH = 6, // 1 KB Cahe size 
-	parameter BLOCK_OFFSET_WIDTH = 2
-	)(
-	// General signals
-	input clk,    // Clock
-	input rst_n,  // Synchronous reset active low
+ module i_cache #(
+	 parameter INDEX_WIDTH = 6, // 1 KB Cahe size 
+	 parameter BLOCK_OFFSET_WIDTH = 2,
+	 parameter BUFFER_LEN = 8,
+	 parameter ASSOCIATIVITY = 2
+	 )(
+	 // General signals
+	 input clk,    // Clock
+	 input rst_n,  // Synchronous reset active low
+ 
+	 // Request
+	 pc_ifc.in i_pc_current,
+	 pc_ifc.in i_pc_next,
+ 
+	 // Response
+	 cache_output_ifc.out out,
+ 
+	 // Memory interface
+	 axi_read_address.master mem_read_address[BUFFER_LEN],
+	 axi_read_data.master mem_read_data[BUFFER_LEN]
+ );
+ localparam TAG_WIDTH = `ADDR_WIDTH - INDEX_WIDTH - BLOCK_OFFSET_WIDTH - 2;
+ localparam LINE_SIZE = 1 << BLOCK_OFFSET_WIDTH;
+ localparam DEPTH = 1 << INDEX_WIDTH;
 
-	// Request
-	pc_ifc.in i_pc_current,
-	pc_ifc.in i_pc_next,
+ // Check if the parameters are set correctly
+ generate
+	if(TAG_WIDTH <= 0 || LINE_SIZE > 16)
+	begin
+		INVALID_I_CACHE_PARAM invalid_i_cache_param ();
+	end
+endgenerate
 
-	// Response
-	cache_output_ifc.out out,
+ // Parsing
+ logic [TAG_WIDTH - 1 : 0] i_tag;
+ logic [INDEX_WIDTH - 1 : 0] i_index;
+ logic [BLOCK_OFFSET_WIDTH - 1 : 0] i_block_offset;
 
-	// Memory interface
-	axi_read_address.master mem_read_address,
-	axi_read_data.master mem_read_data
+ logic [INDEX_WIDTH - 1 : 0] i_index_next;
+ logic sb_hit;
+ logic [`DATA_WIDTH-1:0] sb_rdata[LINE_SIZE];
+
+ assign {i_tag, i_index, i_block_offset} = i_pc_current.pc[`ADDR_WIDTH - 1 : 2];
+ assign i_index_next = i_pc_next.pc[BLOCK_OFFSET_WIDTH + 2 +: INDEX_WIDTH];
+ // Above line uses +: slice, a feature of SystemVerilog
+ // See https://stackoverflow.com/questions/18067571
+
+ // States
+ enum logic [2:0] {
+	 STATE_READY,            // Ready for incoming requests
+	 STATE_REFILL_REQUEST,   // Sending out memory read request
+	 STATE_REFILL_DATA       // Loads a cache line from memory
+ } state, next_state;
+
+ // Registers for flushing and refilling
+ logic [INDEX_WIDTH - 1:0] r_index;
+ logic [TAG_WIDTH - 1:0] r_tag;
+
+ // databank signals
+ logic [LINE_SIZE - 1 : 0] databank_select;
+ logic [LINE_SIZE - 1 : 0] databank_we[ASSOCIATIVITY];
+ logic [`DATA_WIDTH - 1 : 0] databank_wdata[LINE_SIZE];
+ logic [INDEX_WIDTH - 1 : 0] databank_waddr;
+ logic [INDEX_WIDTH - 1 : 0] databank_raddr;
+ logic [`DATA_WIDTH - 1 : 0] databank_rdata [ASSOCIATIVITY][LINE_SIZE];
+
+ logic select_way;
+ logic r_select_way;
+ logic [DEPTH - 1 : 0] lru_rp;
+
+ // databanks
+ genvar g,w;
+ generate
+	 for (g = 0; g < LINE_SIZE; g++)
+	 begin : datasets
+		 for (w=0; w< ASSOCIATIVITY; w++)
+		 begin : databanks
+			 cache_bank #(
+				 .DATA_WIDTH (`DATA_WIDTH),
+				 .ADDR_WIDTH (INDEX_WIDTH)
+			 ) databank (
+				 .clk,
+				 .i_we (databank_we[w][g]),
+				 .i_wdata(databank_wdata[g]),
+				 .i_waddr(databank_waddr),
+				 .i_raddr(databank_raddr),
+
+				 .o_rdata(databank_rdata[w][g])
+			 );
+		 end
+	 end
+ endgenerate
+
+ // tagbank signals
+ logic tagbank_we[ASSOCIATIVITY];
+ logic [TAG_WIDTH - 1 : 0] tagbank_wdata;
+ logic [INDEX_WIDTH - 1 : 0] tagbank_waddr;
+ logic [INDEX_WIDTH - 1 : 0] tagbank_raddr;
+ logic [TAG_WIDTH - 1 : 0] tagbank_rdata[ASSOCIATIVITY];
+
+ generate
+	 for (w=0; w< ASSOCIATIVITY; w++)
+	 begin: tagbanks
+		 cache_bank #(
+			 .DATA_WIDTH (TAG_WIDTH),
+			 .ADDR_WIDTH (INDEX_WIDTH)
+		 ) tagbank (
+			 .clk,
+			 .i_we    (tagbank_we[w]),
+			 .i_wdata (tagbank_wdata),
+			 .i_waddr (tagbank_waddr),
+			 .i_raddr (tagbank_raddr),
+
+			 .o_rdata (tagbank_rdata[w])
+		 );
+	 end
+ endgenerate
+
+ // Valid bits
+ logic [DEPTH - 1 : 0] valid_bits[ASSOCIATIVITY];
+
+
+ // Intermediate signals
+ logic hit, miss, tag_hit;
+ logic last_refill_word;
+
+ always_comb
+ begin
+	 tag_hit = ( ((i_tag == tagbank_rdata[0]) & valid_bits[0][i_index])
+			   |	((i_tag == tagbank_rdata[1]) & valid_bits[1][i_index]));
+	 hit = (tag_hit)
+		 & (state == STATE_READY);
+	 miss = ~hit;
+ 
+	 if (hit)
+	 begin
+		 if (i_tag == tagbank_rdata[0])
+		 begin
+			 select_way = 'b0;
+		 end
+		 else 
+		 begin
+			 select_way = 'b1;
+		 end
+	 end
+	 else if (miss)
+	 begin
+		 select_way = lru_rp[i_index];
+	 end
+	 else
+	 begin
+		 select_way = 'b0;
+	 end
+ 
+ end
+
+ always_comb
+ begin
+	 for (int i=0; i<ASSOCIATIVITY;i++)
+		 databank_we[i] = '0;
+	 if (sb_hit)				// We are refilling data
+		 databank_we[r_select_way] = {LINE_SIZE{sb_hit & (state == STATE_REFILL_DATA)}};
+ end
+
+ always_comb
+ begin
+
+		 databank_wdata = sb_rdata;
+		 databank_waddr = r_index;
+		 if (next_state == STATE_READY)
+			 databank_raddr = i_index_next;
+		 else
+			 databank_raddr = r_index;
+ end
+
+ always_comb
+ begin
+	 tagbank_we[r_select_way] = sb_hit & (state == STATE_REFILL_DATA);
+	 tagbank_we[~r_select_way] = '0;
+	 tagbank_wdata = r_tag;
+	 tagbank_waddr = r_index;
+	 tagbank_raddr = i_index_next;
+ end
+
+ always_comb
+ begin
+	 out.valid = hit;
+	 out.data = databank_rdata[select_way][i_block_offset];
+ end
+
+ always_comb
+ begin
+	 next_state = state;
+	 unique case (state)
+		 STATE_READY:
+			 if (miss)
+				next_state = STATE_REFILL_DATA;
+		 STATE_REFILL_DATA:
+			 if (sb_hit)
+				 next_state = STATE_READY;
+	 endcase
+ end
+
+ always_ff @(posedge clk)
+ begin
+	 if(~rst_n)
+	 begin
+		 state <= STATE_READY;
+		 databank_select <= 1;
+		 for (int i=0; i<ASSOCIATIVITY;i++)
+			 valid_bits[i] <= '0;
+		 for (int i=0; i<DEPTH;i++)
+			 lru_rp[i] <= 0;
+	 end
+	 else
+	 begin
+		 state <= next_state;
+
+		 case (state)
+			 STATE_READY:
+			 begin
+				 if (miss)
+				 begin
+					 r_tag <= i_tag;
+					 r_index <= i_index;
+					 r_select_way <= select_way;
+				 end
+					 lru_rp[i_index] <= ~select_way;
+			 end
+			 STATE_REFILL_DATA:
+			 begin
+				 if (sb_hit)
+				 begin
+					 valid_bits[r_select_way][r_index] <= 1'b1;
+				 end
+			 end
+		 endcase
+	 end
+ end
+ stream_buffer#(
+	.BLOCK_OFFSET_WIDTH(BLOCK_OFFSET_WIDTH),
+	.LINE_SIZE(LINE_SIZE),
+	.BUFF_DATA_WIDTH(`DATA_WIDTH),
+	.BUFFER_LEN(BUFFER_LEN),
+	.MEMID(0)
+)SB_G(
+	.clk,
+	.rst_n(rst_n),
+	.current_addr({r_tag,r_index,2'b0}),
+	.cache_miss(miss),
+	.miss_valid(STATE_REFILL_DATA == state),
+	.hit_out(sb_hit),
+	.sb_rdata(sb_rdata),
+	.mem_read_address,
+	.mem_read_data
 );
-	localparam TAG_WIDTH = `ADDR_WIDTH - INDEX_WIDTH - BLOCK_OFFSET_WIDTH - 2;
-	localparam LINE_SIZE = 1 << BLOCK_OFFSET_WIDTH;
-	localparam DEPTH = 1 << INDEX_WIDTH;
-
-	// Check if the parameters are set correctly
-	generate
-		if(TAG_WIDTH <= 0 || LINE_SIZE > 16)
-		begin
-			INVALID_I_CACHE_PARAM invalid_i_cache_param ();
-		end
-	endgenerate
-
-	// Parsing
-	logic [TAG_WIDTH - 1 : 0] i_tag;
-	logic [INDEX_WIDTH - 1 : 0] i_index;
-	logic [BLOCK_OFFSET_WIDTH - 1 : 0] i_block_offset;
-
-	logic [INDEX_WIDTH - 1 : 0] i_index_next;
-
-	assign {i_tag, i_index, i_block_offset} = i_pc_current.pc[`ADDR_WIDTH - 1 : 2];
-	assign i_index_next = i_pc_next.pc[BLOCK_OFFSET_WIDTH + 2 +: INDEX_WIDTH];
-	// Above line uses +: slice, a feature of SystemVerilog
-	// See https://stackoverflow.com/questions/18067571
-
-	// States
-	enum logic[1:0] {
-		STATE_READY,            // Ready for incoming requests
-		STATE_REFILL_REQUEST,   // Sending out a memory read request
-		STATE_REFILL_DATA       // Missing on a read
-	} state, next_state;
-
-	// Registers for refilling
-	logic [INDEX_WIDTH - 1:0] r_index;
-	logic [TAG_WIDTH - 1:0] r_tag;
-
-	// databank signals
-	logic [LINE_SIZE - 1 : 0] databank_select;
-	logic [LINE_SIZE - 1 : 0] databank_we;
-	logic [`DATA_WIDTH - 1 : 0] databank_wdata;
-	logic [INDEX_WIDTH - 1 : 0] databank_waddr;
-	logic [INDEX_WIDTH - 1 : 0] databank_raddr;
-	logic [`DATA_WIDTH - 1 : 0] databank_rdata [LINE_SIZE];
-
-	// databanks
-	genvar g;
-	generate
-		for (g = 0; g < LINE_SIZE; g++)
-		begin : databanks
-			cache_bank #(
-				.DATA_WIDTH (`DATA_WIDTH),
-				.ADDR_WIDTH (INDEX_WIDTH)
-			) databank (
-				.clk,
-				.i_we (databank_we[g]),
-				.i_wdata(databank_wdata),
-				.i_waddr(databank_waddr),
-				.i_raddr(databank_raddr),
-
-				.o_rdata(databank_rdata[g])
-			);
-		end
-	endgenerate
-
-	// tagbank signals
-	logic tagbank_we;
-	logic [TAG_WIDTH - 1 : 0] tagbank_wdata;
-	logic [INDEX_WIDTH - 1 : 0] tagbank_waddr;
-	logic [INDEX_WIDTH - 1 : 0] tagbank_raddr;
-	logic [TAG_WIDTH - 1 : 0] tagbank_rdata;
-
-	cache_bank #(
-		.DATA_WIDTH (TAG_WIDTH),
-		.ADDR_WIDTH (INDEX_WIDTH)
-	) tagbank (
-		.clk,
-		.i_we    (tagbank_we),
-		.i_wdata (tagbank_wdata),
-		.i_waddr (tagbank_waddr),
-		.i_raddr (tagbank_raddr),
-
-		.o_rdata (tagbank_rdata)
-	);
-
-	// Valid bits
-	logic [DEPTH - 1 : 0] valid_bits;
-
-	// Intermediate signals
-	logic hit, miss;
-	logic last_refill_word;
-
-
-	always_comb
-	begin
-		hit = valid_bits[i_index]
-			& (i_tag == tagbank_rdata)
-			& (state == STATE_READY);
-		miss = ~hit;
-		last_refill_word = databank_select[LINE_SIZE - 1]
-			& mem_read_data.RVALID;
-	end
-
-	always_comb
-	begin
-		mem_read_address.ARADDR = {r_tag, r_index,
-			{BLOCK_OFFSET_WIDTH + 2{1'b0}}};
-		mem_read_address.ARLEN = LINE_SIZE;
-		mem_read_address.ARVALID = state == STATE_REFILL_REQUEST;
-		mem_read_address.ARID = 4'd0;
-
-		// Always ready to consume data
-		mem_read_data.RREADY = 1'b1;
-	end
-
-	always_comb
-	begin
-		if (mem_read_data.RVALID)
-			databank_we = databank_select;
-		else
-			databank_we = '0;
-
-		databank_wdata = mem_read_data.RDATA;
-		databank_waddr = r_index;
-		databank_raddr = i_index_next;
-	end
-
-	always_comb
-	begin
-		tagbank_we = last_refill_word;
-		tagbank_wdata = r_tag;
-		tagbank_waddr = r_index;
-		tagbank_raddr = i_index_next;
-	end
-
-	always_comb
-	begin
-		out.valid = hit;
-		out.data = databank_rdata[i_block_offset];
-	end
-
-	always_comb
-	begin
-		next_state = state;
-		unique case (state)
-			STATE_READY:
-				if (miss)
-					next_state = STATE_REFILL_REQUEST;
-			STATE_REFILL_REQUEST:
-				if (mem_read_address.ARREADY)
-					next_state = STATE_REFILL_DATA;
-			STATE_REFILL_DATA:
-				if (last_refill_word)
-					next_state = STATE_READY;
-		endcase
-	end
-
-	always_ff @(posedge clk)
-	begin
-		if(~rst_n)
-		begin
-			state <= STATE_READY;
-			databank_select <= 1;
-			valid_bits <= '0;
-		end
-		else
-		begin
-			state <= next_state;
-
-			case (state)
-				STATE_READY:
-				begin
-					if (miss)
-					begin
-						r_tag <= i_tag;
-						r_index <= i_index;
-					end
-				end
-				STATE_REFILL_REQUEST:
-				begin
-				end
-				STATE_REFILL_DATA:
-				begin
-					if (mem_read_data.RVALID)
-					begin
-						databank_select <= {databank_select[LINE_SIZE - 2 : 0],
-							databank_select[LINE_SIZE - 1]};
-						valid_bits[r_index] <= last_refill_word;
-					end
-				end
-			endcase
-		end
-	end
-endmodule
+ endmodule
+ 
+ 
--- mips_cpu/mips_core/mips_core_interfaces.sv
+++ mips_cpu/mips_core/mips_core_interfaces.sv
@@ -12,24 +12,27 @@
 interface load_pc_ifc ();
 	logic we;	// Write Enable
 	logic [`ADDR_WIDTH - 1 : 0] new_pc;
+	logic thread_id;
 
-	modport in  (input we, new_pc);
-	modport out (output we, new_pc);
+	modport in  (input we, new_pc,thread_id);
+	modport out (output we, new_pc,thread_id);
 endinterface
 
 interface pc_ifc ();
 	logic [`ADDR_WIDTH - 1 : 0] pc;
+	logic thread_id;
 
-	modport in  (input pc);
-	modport out (output pc);
+	modport in  (input pc,thread_id);
+	modport out (output pc,thread_id);
 endinterface
 
 interface cache_output_ifc ();
 	logic valid;	// Output Valid
 	logic [`DATA_WIDTH - 1 : 0] data;
+	logic thread_id;
 
-	modport in  (input valid, data);
-	modport out (output valid, data);
+	modport in  (input valid, data,thread_id);
+	modport out (output valid, data,thread_id);
 endinterface
 
 // Note: Similar to branch_decoded_ifc
@@ -44,8 +47,10 @@ interface branch_prediction_ifc ();
 
 	mips_core_pkg::BranchOutcome prediction;
 
-	modport in (input valid, target, prediction);
-	modport out (output valid, target, prediction);
+	logic thread_id;
+
+	modport in (input valid, target, prediction,thread_id);
+	modport out (output valid, target, prediction,thread_id);
 endinterface
 
 // Note: Replacement for branch_result_ifc.
@@ -74,8 +79,10 @@ interface alu_pass_through_ifc ();
 	logic uses_rw;
 	mips_core_pkg::MipsReg rw_addr;
 
-	modport in  (input is_mem_access, mem_action, sw_data, uses_rw, rw_addr);
-	modport out (output is_mem_access, mem_action, sw_data, uses_rw, rw_addr);
+	logic thread_id;
+
+	modport in  (input is_mem_access, mem_action, sw_data, uses_rw, rw_addr,thread_id);
+	modport out (output is_mem_access, mem_action, sw_data, uses_rw, rw_addr,thread_id);
 endinterface
 
 // This interface is no longer needed as branch evaluation happens in decode.
@@ -96,24 +103,28 @@ interface d_cache_pass_through_ifc ();
 	logic uses_rw;
 	mips_core_pkg::MipsReg rw_addr;
 
-	modport in  (input is_mem_access, alu_result, uses_rw, rw_addr);
-	modport out (output is_mem_access, alu_result, uses_rw, rw_addr);
+	logic thread_id;
+
+	modport in  (input is_mem_access, alu_result, uses_rw, rw_addr,thread_id);
+	modport out (output is_mem_access, alu_result, uses_rw, rw_addr,thread_id);
 endinterface
 
 interface write_back_ifc ();
 	logic uses_rw;	// Write Enable
 	mips_core_pkg::MipsReg rw_addr;
 	logic [`DATA_WIDTH - 1 : 0] rw_data;
+	logic thread_id;
 
-	modport in  (input uses_rw, rw_addr, rw_data);
-	modport out (output uses_rw, rw_addr, rw_data);
+	modport in  (input uses_rw, rw_addr, rw_data,thread_id);
+	modport out (output uses_rw, rw_addr, rw_data,thread_id);
 endinterface
 
 interface hazard_control_ifc ();
 	// Stall signal has higher priority
 	logic flush;	// Flush signal of the previous stage
 	logic stall;	// Stall signal of the next stage
+	logic thread_id;
 
-	modport in  (input flush, stall);
-	modport out (output flush, stall);
+	modport in  (input flush, stall,thread_id);
+	modport out (output flush, stall,thread_id);
 endinterface
